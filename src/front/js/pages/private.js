ffcb8a0051e606d8c541b90caa5be7cab41a47fe
PatriciaMG92 <xiqitinah_92@hotmail.com>
2023-04-14 07:54:35 +0000

    first commit

---
+++ src/front/js/pages/private.js
@@ -0,0 +1,12 @@
+import React, { useState, useEffect, useContext } from "react";
+import { Link, useParams } from "react-router-dom";
+import { Context } from "../store/appContext";
+import rigoImageUrl from "../../img/rigo-baby.jpg";
+
+export const Private = () =>{
+    return (
+        <>
+        <h1>Hi</h1>
+        </>
+    )
+}
